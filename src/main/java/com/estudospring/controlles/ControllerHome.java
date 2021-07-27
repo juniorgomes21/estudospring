d774e17e55e966ca7cd129d046cf5ffafbbc81ce
Junior Gomes <alamviegas.155@gmail.com>
2021-07-27 12:47:38 -0300

    fragment_ok

---
+++ src/main/java/com/estudospring/controlles/ControllerHome.java
@@ -0,0 +1,14 @@
+package com.estudospring.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerHome {
+
+    @GetMapping("/")
+    public String home() {
+        
+        return "home";
+    }
+}
